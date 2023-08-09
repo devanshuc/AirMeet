df14846576e11e3914f4e3dc9c8c0e7ef07cd60a
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-08-09 15:38:34 +0530

    Options Component

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -9,8 +9,9 @@ import {
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { SocketContext } from "../Context";
 import { useContext, useState } from "react";
+// import { AssignmentIndIcon } from "@mui/icons-material/AssignmentInd";
 
-const Options = (props) => {
+const Options = ({ children }) => {
 	const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
 		useContext(SocketContext);
 
@@ -23,6 +24,22 @@ const Options = (props) => {
 					<Grid container>
 						<Grid item xs={12} md={6}>
 							<Typography gutterBottom variant="h6">
+								Make a Call
+							</Typography>
+							<TextField
+								label="Name"
+								value={name}
+								onChange={(e) => setIdToCall(e.target.value)}
+								fullWidth
+							/>
+							<CopyToClipboard text={me}>
+								<Button variant="contained" color="primary" fullWidth>
+									Button
+								</Button>
+							</CopyToClipboard>
+						</Grid>
+						<Grid item xs={12} md={6}>
+							<Typography gutterBottom variant="h6">
 								Account Info
 							</Typography>
 							<TextField
@@ -31,12 +48,32 @@ const Options = (props) => {
 								onChange={(e) => setName(e.target.value)}
 								fullWidth
 							/>
+							{callAccepted && !callEnded ? (
+								<Button
+									variant="contained"
+									color="secondary"
+									fontSize="large"
+									onClick={leaveCall}
+									fullWidth
+								>
+									Hang Up
+								</Button>
+							) : (
+								<Button
+									variant="contained"
+									color="primary"
+									fontSize="large"
+									onClick={() => callUser(idToCall)}
+									fullWidth
+								>
+									Call
+								</Button>
+							)}
 						</Grid>
 					</Grid>
 				</form>
+				{children}
 			</Paper>
-			Options
-			{props.children}
 		</Container>
 	);
 };
